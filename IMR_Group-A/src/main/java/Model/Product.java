102fa9dbf4be9c88daeb0d8e617dbef160796838
Chiran Sadeesa <10952827@students.plymouth.ac.uk>
2025-01-02 21:59:09 +0530

    Few Changes

--- IMR_Group-A/src/main/java/Model/Product.java
+++ IMR_Group-A/src/main/java/Model/Product.java
@@ -22,30 +22,23 @@ public class Product {
     }
 
     public int getProductID() {return productID;}
-    
     public void setProductID(int productID) {this.productID = productID;}
 
     public String getProductName() {return productName;}
-    
     public void setProductName(String productName) {this.productName = productName;}
 
     public String getProductCategory() {return productCategory;}
-    
     public void setProductCategory(String productCategory) {this.productCategory = productCategory;}
 
     public boolean isWeighted() {return isWeighted;}
-    
     public void setWeighted(boolean isWeighted) {this.isWeighted = isWeighted;}
 
     public double getPricePerUnit() {return pricePerUnit;}
-    
     public void setPricePerUnit(double pricePerUnit) {this.pricePerUnit = pricePerUnit;}
 
     public double getPricePer100g() {return pricePer100g;}
-    
     public void setPricePer100g(double pricePer100g) {this.pricePer100g = pricePer100g;}
 
     public String getProductImageURL() {return productImageURL;}
-    
     public void setProductImageURL(String productImageURL) {this.productImageURL = productImageURL;}
 }
